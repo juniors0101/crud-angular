7607c58f92361aa1c1e4490b9feb5d77bdf80fb7
junior sousa <92957053+juniors0101@users.noreply.github.com>
2022-07-05 15:16:01 -0300

    update create

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -24,7 +24,7 @@ export class ProductCreateComponent implements OnInit {
 
   createProduct(): void {
     this.productService.create(this.product).subscribe(() => {
-      this.productService.showMessage('Produto Criado!')
+      this.productService.showMessage('Produto cadastrado com sucesso!')
       this.router.navigate(['/products'])
     })
 
